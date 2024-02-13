609d75605b7c05f9140e9740461cf0da78e766e4
Ines Meddeb <ines.meddeb025@gmail.com>
2024-02-13 17:42:35 +0100

    Player + the camera following it

---
+++ Assets/Scripts/PlayerMovment.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float speed = 5;
+    public Rigidbody rb;
+    float horizentalInput; 
+    public float horizontalMultiplayer = 2;
+
+    private void FixedUpdate ()
+    {
+        Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
+        Vector3 horizentalMove = transform.right * horizentalInput * speed * Time.fixedDeltaTime * horizontalMultiplayer;
+        rb.MovePosition(rb.position + forwardMove + horizentalMove);
+
+    }
+
+
+    // Update is called once per frame
+    private void Update()
+    {
+        horizentalInput = Input.GetAxis("Horizontal");
+    }
+}
